599db33a074eccb6468a3743c9e614bbac0660e0
Bluette1 <marylene.sawyer@gmail.com>
2021-03-31 11:53:14 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const { graphqlHTTP } = require("express-graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "HelloWorld",
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => "Hello world",
+      },
+    }),
+  }),
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphql: true,
+    schema: schema,
+  })
+);
+
+app.listen(5000, () => console.log("Server Running"));
